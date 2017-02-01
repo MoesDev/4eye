8a31824fbb06b22d36116ae92a86af65e55b34a1
Danyal Khan <danyalkhan11@gmail.com>
2017-02-01 16:31:12 -0600

    added new files

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyparser = require('body-parser');
+
+// var client = require('guidebox')
+// var Guidebox = new client('4fdc1c317df16eac622649a5cf94f8b2b49b5e74');
+// movies = Guidebox.movies.list();
+
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, "./client")));
+app.use(express.static(path.join(__dirname, './bower_components')));
+
+
+app.listen(8000, function(){
+    console.log('listening on port 8000');
+});
